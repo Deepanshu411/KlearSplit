50094e329533a14b1e6b4d4c1f7c94599c973da8
Deepanshu Dixit <deepanshu6255@gmail.com>
2024-09-23 19:57:49 +0530

    Created Login and Register page UI with Bootstrap

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RegisterComponent } from "./components/authComponents/register/register.component";
+import { LoginComponent } from "./components/authComponents/login/login.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RegisterComponent, LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- server/src/api/v1/middlewares/auth.middleware.js
+++ server/src/api/v1/middlewares/auth.middleware.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-undef */
 
 import jwt from "jsonwebtoken";
+import { generateAccessToken } from "../utils/jwt.util.js";
 
 const authenticate = (req, res, next) => {
   const accessToken = req.headers["authorization"]?.split(" ")[1];
@@ -15,28 +16,30 @@ const authenticate = (req, res, next) => {
     req.user = decoded.user;
     next();
   } catch (error) {
-    console.log(error);
+    console.log(error, 'gyuj');
     if (!refreshToken) {
-      return res.status(401).send("Access Denied. No refresh token provided.");
+      return res.status(401).json({error: "Access Denied. No refresh token provided."});
     }
 
     try {
       const decoded = jwt.verify(refreshToken, process.env.REFRESH_SECRET_KEY);
-      const accessToken = jwt.sign(
-        { user: decoded.user },
-        process.env.ACCESS_SECRET_KEY,
-        { expiresIn: "15m" },
-      );
+      const accessToken = generateAccessToken(decoded.user);
+      const refreshToken = generateAccessToken(decoded.user);
+
+      res
+        .cookie("accessToken", accessToken, {
+          httpOnly: true,
+          sameSite: "strict",
+        })
+        .header("Authorization", accessToken)
 
       res
         .cookie("refreshToken", refreshToken, {
           httpOnly: true,
           sameSite: "strict",
         })
-        .header("Authorization", accessToken)
-        .send(decoded.user);
     } catch (error) {
-      console.log(error);
+      console.log(error, 'hgffghfh');
       return res.status(400).send("Invalid Token.");
     }
   }
